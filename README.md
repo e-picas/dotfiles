fab5611ec6ec6a1fb55493c126d28ba6b6c13679
picas <me@picas.fr>
2016-02-16 16:55:44 +0100

    wip of the day

--- README.md
+++ README.md
@@ -26,46 +26,65 @@ To see installer usage, run:
 Usage
 -----
 
-List of different filesystem treatments:
+### List of special files
 
-`<filename>.model`
-:   this identifies a file containing some tags or fields to full fill ; this kind of file is
-    hard copied in its final place and then opened in an editor to let you fill required tags
+`.env`
+:   this file may embed a list of variables to load in current environment ; these
+    variables will finally be used by the *models parser* (see below) ; the file must be 
+    written like shell scripts variables:
+    
+        export MY_VAR='my value'
+
+### List of different filesystem treatments
+
+By default, any file is treated following the default action in use (link or hard copy).
+
+By default, any directory is scanned and its contents will be treated with a target path 
+including the relative path of the file (`data/my/subdir/path/my-file` will be linked or 
+copied into `<target>/my/subdir/path/my-file`).
+
+`<dirname>.dir`
+:   this identifies a directory that must be treated "as-is" ; it will be linked or copied (with
+    its contents of course) without analyzing its contents.
 
 `<filename>.enc`
 :   this identifies an encrypted file ; this kind of file is hard copied to its final place and
-    then decrypted with your password (prompted each time)
+    then decrypted with your password (prompted each time).
 
-`<dirname>.dir`
-:   this indifies a directory that must be treated "as-is" ; it will be linked or copied (with
-    its contents of course) without analyzing its contents
+`<filename>.model`
+:   this identifies a file containing some tags or fields to replace ; this kind of file is
+    hard copied in its final place after being parsed to replace tags like `%TAGNAME%` by their
+    values in current environment (see the `.env` file above) and then opened in an editor to let 
+    you verify and fulfill its content.
 
-`<filename>`
-:   any other file is treated following the default action in use (link or hard copy)
+`<filename>.run`
+:   this identifies a script which will be called when installing ; it is first copied at its
+    target place before to be executed (relative paths are possible) ; it is executed without
+    specifying an interpreter, you need to specify it manually as its "shebang".
 
-
-List of local directories and their handling:
+### List of local directories and their handling
 
 `home/`
 :   the place for the *dotfiles* ; they are finally linked or copied at the root of the `<target>/`
-    directory
+    directory ; the contents of this directory are scanned following all the rules described above
+    (for instance, to use a `.vim` directory, you must name it `home/.vim.dir/`).
 
 `bin/`
 :   the place for the binary scripts ; they are finally linked or copied to the `<target>/bin/`
-    directory
+    directory ; all its contents are treated as files (sub-directories will be linked).
 
 `keys/`
 :   the place for the various personal keys you use ; they should be stored encrypted and will
     be finally copied and decrypted in the relative sub-directory of the `<target>/` directory ;
     the contents of this directory are considered "per-file", which means that only files are
     copied, in the path constructed relative to `<target>/` (`keys/.ssh/my_rsa_key.encrypted`
-    will be copied into `<target>/.ssh/my_rsa_key`)
+    will be copied into `<target>/.ssh/my_rsa_key`).
 
 `data/`
 :   the place for any other contents ; they are finally linked or copied to concerned `<target>/<path>/`
     directory ; to link or copy a whole directory, use the `.dir` suffix ; examples:
     -   `data/toughts/my-thought.txt` will be linked to `<target>/toughts/my-thought.txt`
-    -   `data/notes.dir/` will be linked to `<target>/notes/`
+    -   `data/notes.dir/` will be linked to `<target>/notes/`.
 
 Best practices
 --------------
@@ -77,3 +96,5 @@ modifying anything in your clone byt the submodule revision.
 Another best practice for *dotfiles* is to make each of them include a `<dotfile>_alt` file (if it is present)
 to allow "per-device" customizations.
 
+Concerning model files (`<filename>.model`), the best practice is to let user easily identify the strings to
+edit. You can do so by writing something like `%YOUR TAG INFO%`. Keep in mind that, if you have an environment
--- install.sh
+++ install.sh
@@ -8,6 +8,27 @@
 set -e
 
 #######################################################################
+# global paths (update here to modify script's behavior)
+
+declare -rx ENV_FILE='.env'
+declare -rx DOTFILES_DIR='home'
+declare -rx BINARIES_DIR='bin'
+declare -rx MODULES_DIR='modules'
+declare -rx KEYS_DIR='keys'
+declare -rx DATA_DIR='data'
+declare -rx MODELS_EXT='model'
+declare -rx DIRS_EXT='dir'
+declare -rx ENCRYPTED_EXT='enc'
+# %TAG_NAME%
+declare -rx TAG_MASK='%[A-Za-z0-9\-\_]\+%'
+declare -rx TAG_MASK_OPENER='%'
+declare -rx TAG_MASK_CLOSER='%'
+
+#declare -rx MODELS_MASK="*.${MODELS_EXT}"
+#declare -rx DIRS_MASK="*.${DIRS_EXT}"
+#declare -rx ENCRYPTED_MASK="*.${ENCRYPTED_EXT}"
+
+#######################################################################
 # script info
 declare -rx VERSION="1.0.1"
 declare -rx NAME="picas/dotfiles"
@@ -60,11 +81,21 @@ EOT
 
 #######################################################################
 ## library
+# each method must be exported: export -f fctname
+
+# deep error (from functions when they are not used the right way)
+die() {
+    local msg="${1:-died}"
+    echo "[fatal error] > \"${msg}\" thrown at ${BASH_SOURCE[1]}:${FUNCNAME[1]} at line ${BASH_LINENO[0]}" >&2;
+    exit 1
+}
+export -f die
 
 # resolve a link target
 resolve_link() {
     $(type -p greadlink readlink | head -1) "$1"
 }
+export -f resolve_link
 
 # get an absolute dir path
 abs_dirname() {
@@ -78,58 +109,69 @@ abs_dirname() {
     pwd
     cd "$cwd"
 }
+export -f abs_dirname
 
 # usage and exit
 usage() {
     echo "${USAGE}"
     exit "${1:-0}"
 }
+export -f usage
 
 # version and exit
 version() {
     echo "${NAME} ${VERSION} (${REPO})"
     exit "${1:-0}"
 }
+export -f version
 
 # synopsis and exit
 synopsis() {
     echo "${SYNOPSIS}"
     exit "${1:-0}"
 }
+export -f synopsis
 
 # add a log entry
 logger() {
     echo "[$(date +"%Y/%m/%d:%H:%M:%S %z")] [$0] $@" >> "$LOG_FILE";
 }
+export -f logger
 
 # info string (no exit) - to STDERR
 verbose_echo() {
-    $VERBOSE && info "[info]: $@";
+    if $VERBOSE; then
+        info "[info]: $@";
+    fi
 }
+export -f verbose_echo
 
 # info string (no exit) - to STDERR
 info() {
     {   echo
         echo -e "> $@"
-    } >&2 ;
+    } >&2;
 }
+export -f info
 
 # warning string (no exit) - to STDERR
 warning() {
     {   echo
-        echo -e "> [warning]: $@"
-    } >&2 ;
+        echo -e "!! > [warning]: $@"
+    } >&2;
 }
+export -f warning
 
 # error & exit - to STDERR
 error() {
     {   echo
-        echo -e "> [error]: $@"
+        echo -e "!! > [error]: $@"
         echo
         echo "${SYNOPSIS}"
-    }  >&2
+    }  >&2;
     exit 1
 }
+export -f error
 
 # prompt user for confirmation
 user_confirm() {
@@ -140,6 +182,7 @@ user_confirm() {
     else return 0
     fi
 }
+export -f user_confirm
 
 # prompt user for its password
 get_encryption_password () {
@@ -150,11 +193,12 @@ export -f get_encryption_password
 
 # decrypt an encrypted file
 decrypt_file() {
+    [ $# -lt 1 ] && die 'usage: `decrypt_file <filepath>`';
     local f="$1"
-    [ ! -f "$f" ] && error "file '$f' not found!";
+    [ ! -f "$f" ] && die "file '$f' not found!";
     local ofd="$(dirname "$f")"
     local ofn="$(basename "$f")"
-    local tfn="${ofn/.encrypted}"
+    local tfn="${ofn/.${ENCRYPTED_EXT}}"
     local p="$(get_encryption_password "${tfn}" < /dev/tty; echo)"
     openssl enc -aes-256-cbc -a -salt -pass "pass:$p" -in "$f" -out "${ofd}/${tfn}" -d 2> /dev/null && rm -f "$f";
 }
@@ -162,43 +206,102 @@ export -f decrypt_file
 
 # encrypt a file
 encrypt_file() {
+    [ $# -lt 1 ] && die 'usage: `encrypt_file <filepath>`';
     local f="$1"
-    [ ! -f "$f" ] && error "file '$f' not found!";
+    [ ! -f "$f" ] && die "file '$f' not found!";
     local ofd="$(dirname "$f")"
     local ofn="$(basename "$f")"
-    local tfn="${ofn}.encrypted"
+    local tfn="${ofn}.${ENCRYPTED_EXT}"
     local p="$(get_encryption_password "${ofn}" < /dev/tty; echo)"
     openssl enc -aes-256-cbc -a -salt -pass "pass:$p" -in "$f" -out "${ofd}/${tfn}" 2> /dev/null && rm -f "$f";
 }
 export -f encrypt_file
 
-# symlink or copy any file but a model
-process_dotfile() {
-    local f="$1"
-    [ ! -f "$f" ] && error "file '$f' not found!";
-    if [ "${INSTALL_MODE}" == 'hard-copies' ]
-    then
-        cp ${CMDOPTS_CP} "${f}" "${INSTALL_DIR}/";
-    else
-        ln ${CMDOPTS_LN} "${f}" "${INSTALL_DIR}/";
+path_exists() {
+    local p="$1"
+    if [ -f "$p" ]||[ -d "$p" ]||[ -h "$p" ]; then
+        return 0
     fi
+    return 1;
 }
-export -f process_dotfile
+export -f path_exists
+
+get_relative_source_path() {
+    [ $# -lt 1 ] && die 'usage: `get_relative_source_path <filepath>`';
+    echo "${1//${INSTALL_DIR}\//}";
+}
+export -f get_relative_source_path
+
+get_relative_target_path() {
+    [ $# -lt 1 ] && die 'usage: `get_relative_target_path <filepath>`';
+    echo "${1//${TARGET}\//}";
+}
+export -f get_relative_target_path
+
+copy_file() {
+    [ $# -lt 2 ] && die 'usage: `copy_file <source_path> <target_path>`';
+    local source="$1"
+    path_exists "$source" || die "source path '$source' not found!";
+    local target="$2"
+    cp ${CMDOPTS_CP} -r "${source}" "${target}";
+    return $?;
+}
+export -f copy_file
+
+symlink_file() {
+    [ $# -lt 2 ] && die 'usage: `symlink_file <source_path> <target_path>`';
+    local source="$1"
+    path_exists "$source" || die "path '$source' not found!";
+    local target="$2"
+    ln ${CMDOPTS_LN} "${source}" "${target}";
+    return $?;
+}
+export -f symlink_file
+
+# parse tags like `%TAGNAME%` in a file
+parse_tags_in_file() {
+    local f="$1"
+    [ ! -f "$f" ] && die "file '$f' not found!";
+    tags=($(grep -o "${TAG_MASK}" "$f"))
+    for tag in "${tags[@]}"; do
+        varname="${tag:${#TAG_MASK_OPENER}:-${#TAG_MASK_CLOSER}}"
+        [ -n "${!varname}" ] && sed -i "s/${tag}/${!varname}/g" "$f";
+    done
+}
+export -f parse_tags_in_file
 
 # copy and edit a model
 process_model() {
     local f="$1"
-    [ ! -f "$f" ] && error "file '$f' not found!";
+    [ ! -f "$f" ] && die "file '$f' not found!";
     local ofn="$(basename "$f")"
-    local tfn="${ofn/.model}"
-    cp ${CMDOPTS_CP} "${f}" "${INSTALL_DIR}/${tfn}" && vim "${INSTALL_DIR}/${tfn}" < /dev/tty;
+    local tfn="${ofn/.${MODELS_EXT}}"
+    cp ${CMDOPTS_CP} "${f}" "${INSTALL_DIR}/${tfn}";
+    parse_tags_in_file "${INSTALL_DIR}/${tfn}";
+    vim "${INSTALL_DIR}/${tfn}" < /dev/tty;
 }
 export -f process_model
+
+# symlink or copy any file
+process_default() {
+    local f="$1"
+
+    echo "$(path_exists "$f")";
+
+    path_exists "$f" || die "path '$f' not found!";
+    if [ "${INSTALL_MODE}" == 'hard-copies' ]
+    then
+        copy_file "${f}" "${INSTALL_DIR}/$(get_relative_source_path "$f")";
+    else
+        symlink_file "${f}" "${INSTALL_DIR}/$(get_relative_source_path "$(dirname "$f")")";
+    fi
+}
+export -f process_default
 
 # copy and decrypt an encrypted file
 process_encrypted() {
     local f="$1"
-    [ ! -f "$f" ] && error "file '$f' not found!";
+    path_exists "$f" || die "path '$f' not found!";
     local ofn="$(basename "$f")"
     cp ${CMDOPTS_CP} "${f}" "${INSTALL_DIR}/${ofn}";
     decrypt_file "${INSTALL_DIR}/${ofn}"
@@ -208,14 +311,10 @@ export -f process_encrypted
 # installation of a binary
 process_binary() {
     local f="$1"
-    [ ! -e "$f" ] && error "path '$f' not found!";
-    [ ! -d "${INSTALL_DIR}/bin" ] && mkdir -p "${INSTALL_DIR}/bin";
-    if [ "${INSTALL_MODE}" == 'hard-copies' ]
-    then
-        cp ${CMDOPTS_CP} -r "${f}" "${INSTALL_DIR}/bin/";
-    else
-        ln ${CMDOPTS_LN} "${f}" "${INSTALL_DIR}/bin/";
-    fi
+    path_exists "$f" || die "path '$f' not found!";
+    [ ! -d "${INSTALL_DIR}/${BINARIES_DIR}" ] && mkdir -p "${INSTALL_DIR}/${BINARIES_DIR}";
+    process_default "$f"
+    chmod a+x "${INSTALL_DIR}/$(get_relative_source_path "$f")"
 }
 export -f process_binary
 
@@ -224,21 +323,39 @@ process_subdir() {
     local f="$1"
     [ ! -d "$f" ] && error "directory '$f' not found!";
     local odn="$(basename "$f")"
-    local installer="${d}/${AUTOINSTALLER}"
-    if [ ! -f "${installer}" ]
-    then
-        if [ "${INSTALL_MODE}" == 'hard-copies' ]
-        then
-            cp ${CMDOPTS_CP} -r "${SOURCE_DIR}/${odn}" "${INSTALL_DIR}/";
-        else
-            ln ${CMDOPTS_LN} "${SOURCE_DIR}/${odn}" "${INSTALL_DIR}/";
-        fi
-    else
-        $VERBOSE && echo "> calling internal installer in '${odn}' directory ..."
-        mkdir -p "${INSTALL_DIR}/${odn}" && cd "${odn}" && "${installer}" "${INSTALL_DIR}/${odn}";
-    fi
+    process_default "${SOURCE_DIR}/${odn}"
 }
 export -f process_subdir
+
+treat_directory() {
+    local f="$1"
+    [ ! -d "$f" ] && die "source directory '$f' not found!";
+    local rf="$(get_relative_source_path "$f")"
+
+    # copy and edit models
+    verbose_echo "installing model files from '${f}' to '${rf}/' ; first choose models to install then edit them ..."
+    find "$(cd "$f"; pwd)" -path "*.${DIRS_EXT}/*" -o \
+        \( -name "*.${MODELS_EXT}" -prune \) -type f \
+        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_model "$0"' {} \;
+
+    # copy and decrypt encrypted files
+    verbose_echo "installing encrypted files from '${f}' to '${rf}/' ; your password will be required for each file ..."
+    find "$(cd "$f"; pwd)" -path "*.${DIRS_EXT}/*" -o \
+        \( -name "*.${ENCRYPTED_EXT}" -prune \) -type f \
+        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_encrypted "$0"' {} \;
+
+    # copy or link sub-directories
+    verbose_echo "installing sub-directories from '${f}' to '${rf}/' ..."
+    find "$(cd "$f"; pwd)" -name "*.${DIRS_EXT}" -type d \
+        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_subdir "$0"' {} \;
+
+    # symlink all the rest
+    verbose_echo "installing default files from '${f}' to '${rf}/' ..."
+    find "$(cd "$f"; pwd)" -path "*.${DIRS_EXT}/*" -o \
+        \( -name "*.${MODELS_EXT}" -o -name "*.${ENCRYPTED_EXT}" -prune \) -o -type f \
+        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_default "$0"' {} \;
+}
+export -f treat_directory
 
 #######################################################################
 ## arguments
@@ -259,13 +376,11 @@ declare -x CMDOPTS_SSL=''
 
 if [ "$1" == '-h' ]||[ "$1" == '--help' ]||[ "$1" == 'help' ]; then
     usage
-fi
 
-if [ "$1" == '-V' ]||[ "$1" == '--version' ]||[ "$1" == 'version' ]; then
+elif [ "$1" == '-V' ]||[ "$1" == '--version' ]||[ "$1" == 'version' ]; then
     version
-fi
 
-if [ "$1" == '-u' ]||[ "$1" == '--usage' ]||[ "$1" == 'usage' ]; then
+elif [ "$1" == '-u' ]||[ "$1" == '--usage' ]||[ "$1" == 'usage' ]; then
     synopsis
 fi
 
@@ -303,7 +418,7 @@ elif [ "$1" == 'decrypt' ]||[ "$1" == 'd' ]; then
     # target path
     TARGET="${2:-}"
 
-elif [ "$1" == 'encrypt' ]||[ "$1" == 'c' ]; then
+elif [ "$1" == 'encrypt' ]||[ "$1" == 'c' ]||[ "$1" == 'e' ]; then
     INSTALL_TYPE='encrypt'
 
     # target path
@@ -311,7 +426,7 @@ elif [ "$1" == 'encrypt' ]||[ "$1" == 'c' ]; then
 
 else
     # install directory
-    INSTALL_DIR="${1}"
+    INSTALL_DIR="${1%/}"
 
     # type of installation
     INSTALL_TYPE="${2:-all}"
@@ -363,11 +478,11 @@ fi
 # prepare installation
 if [ "${INSTALL_TYPE}" == 'home' ]||[ "${INSTALL_TYPE}" == 'bin' ]||[ "${INSTALL_TYPE}" == 'data' ]||[ "${INSTALL_TYPE}" == 'all' ]; then
 
-    # go to the clone directory
+    # go to the source directory
     verbose_echo "source directory is '${SOURCE_DIR}'"
     cd "${SOURCE_DIR}"
 
-    # check if install directory exists
+    # check if the target directory exists
     if [ ! -d "${INSTALL_DIR}" ]
     then
         error "unknown installation directory '${INSTALL_DIR}'!"
@@ -385,7 +500,7 @@ if [ "${INSTALL_TYPE}" == 'home' ]||[ "${INSTALL_TYPE}" == 'bin' ]||[ "${INSTALL
         fi
     fi
 
-    # update of submodules
+    # update of submodules if required
     if [ -d .git ] && [ -f .gitmodules ]; then
         info "current directory seems to embed GIT sub-modules."
         if user_confirm 'do you want to update them'; then
@@ -396,11 +511,25 @@ if [ "${INSTALL_TYPE}" == 'home' ]||[ "${INSTALL_TYPE}" == 'bin' ]||[ "${INSTALL
         fi
     fi
 
+    # load any .env file
+    if [ -f "${ENV_FILE}" ]; then
+        verbose_echo "loading environment file '${SOURCE_DIR}/${ENV_FILE}'"
+        source "${ENV_FILE}"
+
+        # if you have to check some programs
+        if [ "${#REQUIRED_PROGS[@]}" -gt 0 ]; then
+            for _prog in "${REQUIRED_PROGS[@]}"; do
+                verbose_echo "checking that program '${_prog}' exists"
+                type -P "$_prog" 1>&/dev/null || error "program '${_prog}' is required but is not installed!";
+            done
+        fi
+    fi
+
     # how-to info
     ! $FORCED && {
         info "The installation process will start ... Any prompt is waiting for an answer to validate the action or file:\n\
-          - default answer is ALWAYS 'NO',\n\
-          - you HAVE to type 'y' to validate a prompt.\n\
+    - default answer is ALWAYS 'NO',\n\
+    - you HAVE to type 'y' to validate a prompt.\n\
         ";
     };
 
@@ -408,35 +537,19 @@ fi
 
 # installation of dotfiles
 if [ "${INSTALL_TYPE}" == 'home' ]||[ "${INSTALL_TYPE}" == 'all' ]; then
-
-    # symlink all but models
-    verbose_echo "installing dotfiles in '${INSTALL_DIR}/' ..."
-    find "$(cd home; pwd)" -maxdepth 1 \( -name "*.model" -o -name "*.encrypted" -prune \) -o -type f \
-        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_dotfile "$0"' {} \;
-
-    # copy and edit models
-    verbose_echo  "installing dotfiles models in '${INSTALL_DIR}/': first choose models to install then edit them ..."
-    find "$(cd home; pwd)" -maxdepth 1 -name "*.model" -prune -type f \
-        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_model "$0"' {} \;
-
-    # copy and decrypt encrypted files
-    verbose_echo  "installing encrypted files in '${INSTALL_DIR}/' ; your password will be required ..."
-    find "$(cd home; pwd)" -maxdepth 1 -name "*.encrypted" -prune -type f \
-        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_encrypted "$0"' {} \;
+    treat_directory "${SOURCE_DIR}/${DOTFILES_DIR}"
 fi
 
 # installation of binaries
 if [ "${INSTALL_TYPE}" == 'bin' ]||[ "${INSTALL_TYPE}" == 'all' ]; then
-    verbose_echo  "installing binaries in '${INSTALL_DIR}/bin/' ..."
-    find "$(cd bin; pwd)" -maxdepth 1 \( -name "bin" -o -name "*.model" -prune \) -o \
+    verbose_echo  "installing binaries in '${INSTALL_DIR}/${BINARIES_DIR}/' ..."
+    find "$(cd "${BINARIES_DIR}"; pwd)" -maxdepth 1 \( -name "${BINARIES_DIR}" -o -name "*.${MODELS_EXT}" -prune \) -o \
         ${CMDOPTS_FINDEXECTYPE} bash -c 'process_binary "$0"' {} \;
 fi
 
 # installation of data sub-directories
 if [ "${INSTALL_TYPE}" == 'data' ]||[ "${INSTALL_TYPE}" == 'all' ]; then
-    verbose_echo  "linking sub-directory in '${INSTALL_DIR}/%s' ..."
-    find "$(pwd)" -mindepth 1 -maxdepth 1 \( -name ".git" -o -name "bin" -o -name "home" -o -name "modules" -prune \) -o -type d \
-        ${CMDOPTS_FINDEXECTYPE} bash -c 'process_subdir "$0"' {} \;
+    treat_directory "${SOURCE_DIR}/${DATA_DIR}"
 fi
 
 # done
