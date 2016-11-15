98d83a12121ac5144c62deebb1f8e701d10f2463
picas <me@picas.fr>
2016-11-15 11:32:04 +0100

    re-introduce the 'alert' alias

--- home/.bashrc.d/bash_aliases.sh
+++ home/.bashrc.d/bash_aliases.sh
@@ -65,6 +65,10 @@ else
     alias ls="ls --no-group"
 fi
 
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
 # exclude vcs and IDEs internals from grep
 alias grep='grep --exclude-dir=\.svn --exclude-dir=\.git --exclude-dir=\.idea --exclude-dir=\.settings --exclude=\*\.project --exclude=\*\.sublime\-\* ';
 # special grep for all dev projects
